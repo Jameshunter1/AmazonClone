27614833d600c49bde105469c4a8a513c1569834
[James Hunter] <Jameshunter1017@live.com>
2022-11-08 17:17:46 -0500

    app built

---
+++ src/components/Banner.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+import {Carousel} from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+function Banner() {
+  return (
+      <div className='relative'>
+          <div className='absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-10'/>
+          <Carousel autoplay infiniteLoop showStatus={false} showIndicators={false} showThumbs={false} interval={5000}>
+              <div>
+                  <img loading="lazy" src="https://links.papareact.com/gi1" width="auto" height="auto"/>
+              </div>
+              <div>
+                  <img loading="lazy" src="https://links.papareact.com/6ff" width="auto" height="auto" />
+              </div>
+              <div>
+                  <img loading="lazy" src="https://links.papareact.com/7ma" width="auto" height="auto" />
+              </div>
+              
+          </Carousel>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Header.js
@@ -0,0 +1,72 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+    MagnifyingGlassIcon,
+   Bars3Icon,
+    ShoppingCartIcon,
+} from '@heroicons/react/24/outline';
+
+
+function Header() {
+  return (
+      <header>
+          {/*Top Nav*/}
+          <div className='flex items-center bg-amazon_blue-light px-2 flex-grow py-2'>
+              <div className='mt-2 mx-4 flex items-center flex-grow sm:flex-grow-0'>
+                  <Image src="https://links.papareact.com/f90" width={150} height={600} objectFit='contain' className='cursor-pointer' />
+              </div>
+          
+
+          {/* Search */}
+
+          <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400'>
+                  <input className='flex-grow w-6 h-full flex-shrink rounded-l-md focus:outline-none' type="text" />
+                  <MagnifyingGlassIcon className="h-12 p-4"/>
+              </div>
+              
+
+              {/*Right*/}
+              <div className='flex items-center text-white text-xs space-x-6 mx-6 whitespace-nowrap'>
+                  <div className='link'>
+                      <p> Hello James Hunter</p>
+                      <p className="font-extrabold md:text-sm">Account & Lists</p>
+                  </div>
+
+                  <div className='link'>
+                      <p className="font-extrabold md:text-sm">Returns
+                    </p>
+                      <p className="font-extrabold md:text-sm">& Oders</p>
+                  </div>
+                  <div className='link relative flex items-center'>
+                      <span className='absolute top-0 right-0 h-4 w-4
+                      bg-yellow-400 text-center rounded-full text-black font-bold'>4</span>
+                      <ShoppingCartIcon className='h-10' />
+                      <p className="hidden font-extrabold md:text-sm mt-2">Basket</p>
+                  </div>
+              
+                  
+                  </div>
+          </div>
+          
+          {/*Bottom Nav*/}
+          <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light'>
+              <p className='link flex items-center'>
+                  <Bars3Icon className="cursor-pointer h-6 mr-1" />
+                  All
+              </p>
+              <p className='link'> Prime Video</p>
+              <p className='link'> Amazon Business</p>
+              <p className='link'> Today's Deals</p>
+              <p className='link hidden lg:inline-flex'> Electronics</p>
+              <p className='link hidden lg:inline-flex'> Food & Grocery</p>
+              <p className='link hidden lg:inline-flex'> Prime</p>
+              <p className='link hidden lg:inline-flex'> Buy Again</p>
+              <p className='link hidden lg:inline-flex'> Shopper Toolkit</p>
+              <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
+
+          </div>
+      </header>
+  )
+}
+
+export default Header
